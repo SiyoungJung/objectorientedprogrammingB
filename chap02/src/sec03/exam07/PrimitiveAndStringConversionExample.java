b9af0d3d58e63409d9093d785137b49d009f2716
SiyoungJung <hohahahohaho09@gmail.com>
2025-08-27 09:49:21 +0900

    update chap02, gitignore
    8/25

--- chap02/src/sec03/exam07/PrimitiveAndStringConversionExample.java
+++ chap02/src/sec03/exam07/PrimitiveAndStringConversionExample.java
@@ -12,7 +12,7 @@ public class PrimitiveAndStringConversionExample {
 
         String str1 = String.valueOf(10);
         String str2 = String.valueOf(3.14);
-        String str3 = String.value0f(ture);
+        String str3 = String.valueOf(true);
 
         System.out.println("str1: " + str1);
         System.out.println("str2: " + str2);
---
+++ chap02/src/sec04/exam02/KeycodeExample.java
@@ -0,0 +1,21 @@
+package sec04.exam02;
+
+public class KeycodeExample
+{
+    public static void main(String[] args)
+    {
+        int keyCode;
+
+
+        keyCode = System.in.read();
+        System.out.printf("keyCode: %d\n", keyCode);
+
+        keyCode = System.in.read();
+        System.out.printf("keyCode: %d\n", keyCode);
+
+        keyCode = System.in.read();
+        System.out.printf("keyCode: %d\n", keyCode);
+
+
+    }
+}
