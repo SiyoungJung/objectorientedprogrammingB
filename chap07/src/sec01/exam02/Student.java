72684de3728e5d786edd536d4937e6f024899600
SiyoungJung <hohahahohaho09@gmail.com>
2025-09-24 11:46:14 +0900

    create chap07
    09/24

---
+++ chap07/src/sec01/exam02/Student.java
@@ -0,0 +1,9 @@
+package sec01.exam02;
+
+public class Student extends People {
+    public int studentNO;
+
+    public Student(String name, String ssn, int studentNO) {
+        this.studentNO = studentNO;
+    }
+}
