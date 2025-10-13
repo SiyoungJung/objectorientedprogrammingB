68a1b6df7f5f2524ae4b915ca3497e6d0bb1db79
SiyoungJung <hohahahohaho09@gmail.com>
2025-10-13 12:29:56 +0900

    update chap07
    01/13

--- chap07/src/sec02/exam02/Child.java
+++ chap07/src/sec02/exam02/Child.java
@@ -1,6 +1,6 @@
 package sec02.exam02;
 
-public class Child {
+public class Child extends Parent {
     @Override
     public void method2() {
         System.out.println("Child method2()");
--- chap07/src/sec02/exam02/ChildExample.java
+++ chap07/src/sec02/exam02/ChildExample.java
@@ -1,15 +1,16 @@
 package sec02.exam02;
 
 public class ChildExample {
-    Child child = new Child();
+    public static void main(String[] args) {
+        Child child = new Child();
 
-    Parent parent = new Parent();
-    
-    parent.method1();
-    
-    parent.method2();
-    
+        Parent parent = new Parent();
+
+        parent.method1();
+
+        parent.method2();
+
 //    parent.method3();
 
+        }
 	}
-}
